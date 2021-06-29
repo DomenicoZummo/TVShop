d43789de44bc9753bdd5a9121f97241bc00e3843
PriscoCoppola <priscocoppola7@gmail.com>
2021-06-29 18:23:36 +0200

    add relations

--- resources/views/admin/tvs/create.blade.php
+++ resources/views/admin/tvs/create.blade.php
@@ -24,6 +24,13 @@
         <label for="price">Price</label>
         <input type="number" id="price" name="price" class="form-control">
     </div>
+
+    <div>
+        @foreach ($fields as $field)
+            <input type="checkbox" id="field-{{ $field->id }}" name="fields[]" value="{{ $field->id }}">
+            <label for="">{{ $field->name }}</label>
+        @endforeach
+    </div>
         
 
     <button type="submit" class="btn btn-primary">Create</button>
